25b8541c25cc53d4563b3e6e9d8c4f61e197d39a
neeboo <ghostcorn@gmail.com>
2021-06-03 17:25:03 +0800

    chore(gitignore):remove rust/target

--- rust/target/rls/debug/deps/derivation_path-636176d2830cb712.d
+++
@@ -1,5 +0,0 @@
-/Users/suwei/dev/agent_dart/rust/target/rls/debug/deps/derivation_path-636176d2830cb712.rmeta: /Users/suwei/.cargo/registry/src/github.com-1ecc6299db9ec823/derivation-path-0.1.3/src/lib.rs
-
-/Users/suwei/dev/agent_dart/rust/target/rls/debug/deps/derivation_path-636176d2830cb712.d: /Users/suwei/.cargo/registry/src/github.com-1ecc6299db9ec823/derivation-path-0.1.3/src/lib.rs
-
-/Users/suwei/.cargo/registry/src/github.com-1ecc6299db9ec823/derivation-path-0.1.3/src/lib.rs:
--- rust/target/rls/debug/deps/failure_derive-0a97b636ac4dd145.d
+++
@@ -1,5 +0,0 @@
-/Users/suwei/dev/agent_dart/rust/target/rls/debug/deps/libfailure_derive-0a97b636ac4dd145.dylib: /Users/suwei/.cargo/registry/src/github.com-1ecc6299db9ec823/failure_derive-0.1.8/src/lib.rs
-
-/Users/suwei/dev/agent_dart/rust/target/rls/debug/deps/failure_derive-0a97b636ac4dd145.d: /Users/suwei/.cargo/registry/src/github.com-1ecc6299db9ec823/failure_derive-0.1.8/src/lib.rs
-
-/Users/suwei/.cargo/registry/src/github.com-1ecc6299db9ec823/failure_derive-0.1.8/src/lib.rs:
