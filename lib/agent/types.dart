844d43c4e22503af37faefd602173b3d39362fc3
neeboo <ghostcorn@gmail.com>
2021-06-04 12:58:38 +0800

    feat(cbor):add cbor encode

--- lib/agent/types.dart
+++ lib/agent/types.dart
@@ -1,5 +1,80 @@
-// Copyright (c) 2021 suwei
-// 
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
+import 'dart:typed_data';
+import 'dart:math';
+import 'package:agent_dart/utils/extension.dart';
 
+import 'utils/leb128.dart';
+
+// ignore: constant_identifier_names
+enum BlobType { binary, der, nonce }
+
+abstract class BaseBlob {
+  late final Uint8List _buffer;
+  late BlobType blobType;
+  late String blobName;
+  Uint8List get buffer => _buffer;
+  int get byteLength;
+}
+
+class BinaryBlob extends BaseBlob {
+  BinaryBlob(Uint8List buffer) {
+    _buffer = buffer;
+    blobName = "__BLOB";
+    blobType = BlobType.binary;
+  }
+
+  @override
+  int get byteLength => buffer.lengthInBytes;
+}
+
+class DerEncodedBlob extends BinaryBlob {
+  DerEncodedBlob(Uint8List buffer) : super(buffer) {
+    blobName = "__DER_BLOB";
+    blobType = BlobType.der;
+  }
+}
+
+class Nonce extends BinaryBlob {
+  Nonce(Uint8List buffer) : super(buffer) {
+    blobName = "__nonce__";
+    blobType = BlobType.nonce;
+  }
+}
+
+BinaryBlob blobFromBuffer(ByteBuffer b) {
+  return BinaryBlob(b.asUint8List());
+}
+
+BinaryBlob blobFromUint8Array(Uint8List arr) {
+  return BinaryBlob(arr);
+}
+
+BinaryBlob blobFromText(String text) {
+  return BinaryBlob(text.plainToU8a(useDartEncode: true));
+}
+
+BinaryBlob blobFromUint32Array(Uint32List arr) {
+  return BinaryBlob(arr.buffer.asUint8List());
+}
+
+DerEncodedBlob derBlobFromBlob(BinaryBlob blob) {
+  return DerEncodedBlob(blob.buffer);
+}
+
+BinaryBlob blobFromHex(String hex) {
+  return BinaryBlob(hex.toU8a());
+}
+
+String blobToHex(BinaryBlob blob) {
+  return blob.buffer.toHex(include0x: false);
+}
+
+Uint8List blobToUint8Array(BinaryBlob blob) {
+  return Uint8List.fromList(blob.buffer.sublist(0, blob.byteLength));
+}
+
+Nonce makeNonce() {
+  return Nonce(lebEncode(
+    BigInt.from(DateTime.now().millisecondsSinceEpoch) * BigInt.from(100000) +
+        BigInt.from((Random.secure().nextInt(1) * 100000).floor()),
+  ));
+}
--- lib/agent_dart.dart
+++ lib/agent_dart.dart
@@ -1,2 +1,5 @@
 export 'agent/bls/bls.dart';
+export 'agent/cbor.dart';
+export 'agent/types.dart';
+export 'principal/principal.dart';
 export 'utils/extension.dart';
