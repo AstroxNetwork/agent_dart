5487183410cf00cd2cf67b067dafd38568e47fec
neeboo <ghostcorn@gmail.com>
2021-06-10 00:36:41 +0800

    chore(ios):fix release build

--- lib/agent/bls/ffi_base.dart
+++ lib/agent/bls/ffi_base.dart
@@ -7,15 +7,21 @@ import 'ffi_helper.dart';
 ///
 /// rust func: `fn bls_init() -> bool`
 ///
-typedef RustBlsInitFunc = int Function();
-typedef RustBlsInitNative = Int32 Function();
+typedef RustBlsInitFunc = Pointer<Utf8> Function();
+typedef RustBlsInitNative = Pointer<Utf8> Function();
 final RustBlsInitFunc rustBlsInit =
     dylib.lookup<NativeFunction<RustBlsInitNative>>("bls_init").asFunction();
 
 ///
 /// rust func: `fn bls_init() -> bool`
 ///
-typedef RustBlsVerifyFunc = int Function(Pointer<Utf8> sig, Pointer<Utf8> m, Pointer<Utf8> w);
-typedef RustBlsVerifyNative = Int32 Function(Pointer<Utf8>, Pointer<Utf8>, Pointer<Utf8>);
+typedef RustBlsVerifyFunc = Pointer<Utf8> Function(
+    Pointer<Utf8> sig, Pointer<Utf8> m, Pointer<Utf8> w);
+typedef RustBlsVerifyNative = Pointer<Utf8> Function(Pointer<Utf8>, Pointer<Utf8>, Pointer<Utf8>);
 final RustBlsVerifyFunc rustBlsVerify =
     dylib.lookup<NativeFunction<RustBlsVerifyNative>>("bls_verify").asFunction();
+
+typedef FreeStringFunc = void Function(Pointer<Utf8>);
+typedef FreeStringFuncNative = Void Function(Pointer<Utf8>);
+final FreeStringFunc freeCString =
+    dylib.lookup<NativeFunction<FreeStringFuncNative>>("rust_cstr_free").asFunction();
