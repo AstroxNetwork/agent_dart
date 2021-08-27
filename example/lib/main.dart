91faa594c27192230fcb30830dfc990901b85c92
neeboo <ghostcorn@gmail.com>
2021-08-27 18:37:08 +0800

    feat(version):bump version to 0.1.13

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:agent_dart/agent/auth.dart';
-import 'package:agent_dart/auth_client/webauth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -21,7 +19,7 @@ class _MyAppState extends State<MyApp> {
   int _count = 0;
   bool _loading = false;
   String _status = "";
-  Identity? _identity;
+
   late Counter _counter;
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -39,7 +37,6 @@ class _MyAppState extends State<MyApp> {
       canisterId: "r7inp-6aaaa-aaaaa-aaabq-cai",
       url: "http://127.0.0.1:8000", // For Android emulator, please use 10.0.2.2 as endpoint
       idl: idl,
-      identity: _identity,
     ).hook(Counter());
   }
 
@@ -63,31 +60,6 @@ class _MyAppState extends State<MyApp> {
     readCount();
   }
 
-  void authenticate() async {
-    try {
-      var authClient = WebAuthProvider(
-          scheme: "identity",
-          path: 'auth',
-          authUri: Uri.parse('http://localhost:8080/#authorize'),
-          useLocalPage: false);
-
-      await authClient.login(
-          // AuthClientLoginOptions()..canisterId = "rwlgt-iiaaa-aaaaa-aaaaa-cai"
-          );
-      var loginResult = await authClient.isAuthenticated();
-
-      _identity = authClient.getIdentity();
-
-      setState(() {
-        _status = 'Got result: $loginResult';
-      });
-    } on PlatformException catch (e) {
-      setState(() {
-        _status = 'Got error: $e';
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -102,11 +74,6 @@ class _MyAppState extends State<MyApp> {
             Container(
               height: 30,
             ),
-            ElevatedButton(
-                onPressed: () {
-                  authenticate();
-                },
-                child: const Text("Click here to Login")),
             Container(
               height: 30,
             ),
@@ -114,42 +81,15 @@ class _MyAppState extends State<MyApp> {
             Container(
               height: 30,
             ),
-            Text(_status.isEmpty ? "" : "Principal is ${_identity?.getPrincipal().toText()}"),
           ]),
         ),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: _identity != null
-              ? () async {
-                  increase();
-                }
-              : () {
-                  _showDialog();
-                },
+          onPressed: () async {
+            increase();
+          },
         ),
       ),
-    );
-  }
-
-  void _showDialog() {
-    // flutter defined function
-    showDialog(
-      context: _scaffoldKey.currentContext!,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: const Text("Please login first 🙇‍♂️"),
-          content: const Text("Then try here again, "),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Close me!'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        );
-      },
     );
   }
 }
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import agent_dart
-import flutter_web_auth
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AgentDartPlugin.register(with: registry.registrar(forPlugin: "AgentDartPlugin"))
-  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
