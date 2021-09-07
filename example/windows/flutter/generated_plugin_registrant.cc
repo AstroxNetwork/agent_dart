3509e8bffc2f792ba199837e08fbb733ef249e50
neeboo <ghostcorn@gmail.com>
2021-09-07 10:32:46 +0800

    chore(deps):pubspecs

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -5,11 +5,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <agent_dart/agent_dart_plugin.h>
-#include <url_launcher_windows/url_launcher_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AgentDartPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AgentDartPlugin"));
-  UrlLauncherPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
