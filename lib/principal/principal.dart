fc86c3591e93e0be6ca209d3ffe658f8a16db923
neeboo <ghostcorn@gmail.com>
2021-09-30 16:06:18 +0800

    feat(principal):added principal to accountID

--- lib/principal/principal.dart
+++ lib/principal/principal.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 import 'dart:convert';
+import 'package:agent_dart/utils/extension.dart';
 import 'package:agent_dart/agent/types.dart';
 import 'utils/base32.dart';
 import 'utils/get_crc.dart';
@@ -115,6 +116,19 @@ class Principal {
       throw "no character found";
     }
     return matches.map((e) => e.group(0)).join('-');
+  }
+
+  Uint8List toAccountID() {
+    final hash = SHA224();
+    hash.update(('\x0Aaccount-id').plainToU8a());
+    hash.update(toBlob());
+    hash.update(Uint8List(32));
+    final data = hash.digest();
+    final view = ByteData(4);
+    view.setUint32(0, getCrc32(data.buffer));
+    final checksum = view.buffer.asUint8List();
+    final bytes = Uint8List.fromList(data);
+    return Uint8List.fromList([...checksum, ...bytes]);
   }
 
   @override
