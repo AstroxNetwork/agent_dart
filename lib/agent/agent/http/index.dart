5b313d96b2fc6c3f7de4688b4f68379ef1750f5c
iota9star <iota9star@foxmail.com>
2022-05-04 16:03:05 +0800

    💥 [BC] Fixed fetch method type.

--- lib/agent/agent/http/index.dart
+++ lib/agent/agent/http/index.dart
@@ -1,17 +1,9 @@
 import 'dart:typed_data';
 
-import 'package:agent_dart/agent/agent/api.dart';
 import 'package:agent_dart/agent/agent/http/fetch.dart';
-import 'package:agent_dart/agent/agent/http/transform.dart';
-import 'package:agent_dart/agent/auth.dart';
 import 'package:agent_dart/agent/cbor.dart' as cbor;
 import 'package:agent_dart/agent_dart.dart';
-import 'package:agent_dart/principal/principal.dart';
-import 'package:agent_dart/agent/types.dart';
-import 'package:agent_dart/utils/base64.dart' show base64Encode;
 import 'package:typed_data/typed_data.dart';
-
-import 'types.dart';
 
 const btoa = base64Encode;
 enum FetchMethod { get, post }
@@ -19,14 +11,19 @@ enum FetchMethod { get, post }
 class RequestStatusResponseStatus {
   // ignore: constant_identifier_names
   static const Received = 'received';
+
   // ignore: constant_identifier_names
   static const Processing = 'processing';
+
   // ignore: constant_identifier_names
   static const Replied = 'replied';
+
   // ignore: constant_identifier_names
   static const Rejected = 'rejected';
+
   // ignore: constant_identifier_names
   static const Unknown = 'unknown';
+
   // ignore: constant_identifier_names
   static const Done = 'done';
 }
@@ -38,7 +35,7 @@ const DEFAULT_INGRESS_EXPIRY_DELTA_IN_MSECS = 5 * 60 * 1000;
 // Root public key for the IC, encoded as hex
 // ignore: constant_identifier_names
 const IC_ROOT_KEY =
-    // ignore: prefer_adjacent_string_concatenation
+// ignore: prefer_adjacent_string_concatenation
     '308182301d060d2b0601040182dc7c0503010201060c2b0601040182dc7c05030201036100814' +
         'c0e6ec71fab583b08bd81373c255c3c371b2e84863c98a4f1e08b74235d14fb5d9c0cd546d968' +
         '5f913a0c0b2cc5341583bf4b4392e467db96d65b9bb4cb717112f8472e0d5a4d14505ffd7484' +
@@ -72,12 +69,13 @@ class DefaultHttpAgentOption extends HttpAgentOptions {}
 
 final defaultHttpAgentOption = DefaultHttpAgentOption();
 
-typedef FetchFunc<T> = Future<T> Function(
-    {required String endpoint,
-    String? host,
-    String method,
-    Map<String, dynamic>? headers,
-    dynamic body});
+typedef FetchFunc<T> = Future<T> Function({
+  required String endpoint,
+  String? host,
+  FetchMethod method,
+  Map<String, dynamic>? headers,
+  dynamic body,
+});
 
 // A HTTP agent allows users to interact with a client of the internet computer
 // using the available methods. It exposes an API that closely follows the
@@ -92,6 +90,7 @@ class HttpAgent implements Agent {
   List<HttpAgentRequestTransformFn> _pipeline = [];
 
   late Future<Identity>? _identity;
+
   // _fetch: typeof fetch;
   late String? _host;
   late String? _credentials;
@@ -223,7 +222,7 @@ class HttpAgent implements Agent {
     var list = await Future.wait([
       _fetch!(
           endpoint: "/api/v2/canister/${ecid.toText()}/call",
-          method: "POST",
+          method: FetchMethod.post,
           headers: newTransformed["request"]["headers"],
           body: body),
       Future.value(requestIdOf(submit.toJson()))
@@ -298,7 +297,7 @@ class HttpAgent implements Agent {
 
     final response = await _fetch!(
         endpoint: "/api/v2/canister/${canister.toText()}/query",
-        method: "POST",
+        method: FetchMethod.post,
         headers: newTransformed["request"]["headers"],
         body: body);
 
@@ -347,10 +346,11 @@ class HttpAgent implements Agent {
 
     var body = cbor.cborEncode(newTransformed["body"]);
     final response = await _fetch!(
-        endpoint: "/api/v2/canister/$canister/read_state",
-        method: "POST",
-        headers: newTransformed["request"]["headers"],
-        body: body);
+      endpoint: "/api/v2/canister/$canister/read_state",
+      method: FetchMethod.post,
+      headers: newTransformed["request"]["headers"],
+      body: body,
+    );
 
     if (!(response["ok"] as bool)) {
       // ignore: prefer_adjacent_string_concatenation
@@ -369,8 +369,11 @@ class HttpAgent implements Agent {
 
   @override
   Future<Map> status() async {
-    var response =
-        await _fetch!(endpoint: "/api/v2/status", headers: {}, method: "GET");
+    var response = await _fetch!(
+      endpoint: "/api/v2/status",
+      headers: {},
+      method: FetchMethod.get,
+    );
 
     if (!(response["ok"] as bool)) {
       // ignore: prefer_adjacent_string_concatenation
@@ -392,20 +395,22 @@ class HttpAgent implements Agent {
         : {};
   }
 
-  Future<Map<String, dynamic>> _defaultFetch(
-      {required String endpoint,
-      String? host,
-      String method = "POST",
-      Map<String, dynamic>? headers,
-      dynamic body}) async {
+  Future<Map<String, dynamic>> _defaultFetch({
+    required String endpoint,
+    String? host,
+    FetchMethod method = FetchMethod.post,
+    Map<String, dynamic>? headers,
+    dynamic body,
+  }) async {
     return defaultFetch(
-        endpoint: endpoint,
-        host: host,
-        defaultHost: _host,
-        method: method,
-        headers: headers,
-        baseHeaders: _baseHeaders,
-        body: body);
+      endpoint: endpoint,
+      host: host,
+      defaultHost: _host,
+      method: method,
+      headers: headers,
+      baseHeaders: _baseHeaders,
+      body: body,
+    );
   }
 
   Future<HttpAgentRequest> _transform(HttpAgentRequest request) {
@@ -433,6 +438,7 @@ class HttpAgentReadStateRequest extends HttpAgentQueryRequest {
 
 class HttpAgentCallRequest extends HttpAgentSubmitRequest {
   HttpAgentCallRequest() : super();
+
   @override
   Map<String, dynamic> toJson() {
     return {
