7d3d0ecd0b4de6b19022c79201d765069bf68f22
neeboo <ghostcorn@gmail.com>
2022-05-30 18:54:21 +0800

    :twisted_rightwards_arrows: merge main and fix

--- scripts/build_dart_ffi.sh
+++ scripts/build_dart_ffi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 flutter_rust_bridge_codegen \
 -r rust/src/api.rs \
--d lib/bridge/ffi/bridge_generated.dart \
+# -d lib/bridge/ffi/bridge_generated.dart \
+## there is bug using latest generator
 -c rust/headers/bridge_generated.h
