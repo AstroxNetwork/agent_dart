9bd103eed1d5fc9ba662e8cd6dc39912719bf454
neeboo <ghostcorn@gmail.com>
2022-06-13 20:07:17 +0800

    :green_heart: try fixing ci build on android

--- scripts/build_android_ci.sh
+++ scripts/build_android_ci.sh
@@ -7,22 +7,26 @@ cd rust
 CC_aarch64_linux_android="${ANDROID_PREBUILD_BIN}/aarch64-linux-android${API_LEVEL}-clang" \
 AR_aarch64_linux_android="${ANDROID_PREBUILD_BIN}/aarch64-linux-android-ar" \
 CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="${ANDROID_PREBUILD_BIN}/aarch64-linux-android${API_LEVEL}-clang" \
-  rust-musl-builder cargo build --target aarch64-linux-android --release
+TARGET_CC="${CC_aarch64_linux_android}"
+cargo build --target aarch64-linux-android --release
 
 CC_armv7_linux_androideabi="${ANDROID_PREBUILD_BIN}/armv7a-linux-androideabi${API_LEVEL}-clang" \
 AR_armv7_linux_androideabi="${ANDROID_PREBUILD_BIN}/arm-linux-androideabi-ar" \
 CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="${ANDROID_PREBUILD_BIN}/armv7a-linux-androideabi${API_LEVEL}-clang" \
-  rust-musl-builder cargo build --target armv7-linux-androideabi --release
+TARGET_CC="${CC_armv7_linux_androideabi}"
+cargo build --target armv7-linux-androideabi --release
 
 CC_i686_linux_android="${ANDROID_PREBUILD_BIN}/i686-linux-android${API_LEVEL}-clang" \
 AR_i686_linux_android="${ANDROID_PREBUILD_BIN}/i686-linux-android-ar" \
 CARGO_TARGET_I686_LINUX_ANDROID_LINKER="${ANDROID_PREBUILD_BIN}/i686-linux-android${API_LEVEL}-clang" \
-  rust-musl-builder cargo  build --target i686-linux-android --release
+TARGET_CC="${CC_i686_linux_android}"
+cargo  build --target i686-linux-android --release
 
 CC_x86_64_linux_android="${ANDROID_PREBUILD_BIN}/x86_64-linux-android${API_LEVEL}-clang" \
 AR_x86_64_linux_android="${ANDROID_PREBUILD_BIN}/x86_64-linux-android-ar" \
 CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="${ANDROID_PREBUILD_BIN}/x86_64-linux-android${API_LEVEL}-clang" \
-  rust-musl-builder cargo  build --target x86_64-linux-android --release
+TARGET_CC="${CC_x86_64_linux_android}"
+cargo  build --target x86_64-linux-android --release
 
 for i in "${!ANDROID_ARCHS[@]}";
   do
