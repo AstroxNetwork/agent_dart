4daa9abf50fafe3090e9cc0c1371d793f4f65ddc
Alex Li <github@alexv525.com>
2022-08-20 18:37:26 +0800

    ✨ Allow sub-account for `Principal` -> `AccountId` (#34)
    * ✨ Allow sub-account for `Principal` -> `AccountId`
    Also deprecate `toAccountID` to `toAccountId`.
    
    * 🚀 Merge account ID convert methods

--- lib/wallet/keysmith.dart
+++ lib/wallet/keysmith.dart
@@ -41,25 +41,17 @@ Uint8List getAccountIdFromEd25519PublicKey(Uint8List publicKey) {
   return getAccountIdFromDerKey(der);
 }
 
+@Deprecated('Use Principal.toAccountId instead')
 Uint8List getAccountIdFromPrincipal(Principal principal) {
-  final hash = SHA224();
-  hash.update(('\x0Aaccount-id').plainToU8a());
-  hash.update(principal.toBlob());
-  hash.update(Uint8List(32));
-  final data = hash.digest();
-  final view = ByteData(4);
-  view.setUint32(0, getCrc32(data.buffer));
-  final checksum = view.buffer.asUint8List();
-  final bytes = Uint8List.fromList(data);
-  return Uint8List.fromList([...checksum, ...bytes]);
+  return principal.toAccountId();
 }
 
 Uint8List getAccountIdFromDerKey(Uint8List der) {
-  return getAccountIdFromPrincipal(Principal.selfAuthenticating(der));
+  return Principal.selfAuthenticating(der).toAccountId();
 }
 
 Uint8List getAccountIdFromPrincipalID(String id) {
-  return getAccountIdFromPrincipal(Principal.fromText(id));
+  return Principal.fromText(id).toAccountId();
 }
 
 ECKeys getECKeys(String mnemonic, {String passphase = "", int index = 0}) {
