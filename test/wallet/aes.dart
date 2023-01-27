a511f586a73ded54a76d3074dd28591d5cc2168c
neeboo <ghostcorn@gmail.com>
2023-01-28 02:40:53 +0800

    :sparkles: added p256 encrypt/decrypt message

--- test/wallet/aes.dart
+++ test/wallet/aes.dart
@@ -35,4 +35,33 @@ void main() {
     );
     expect(message, decryptedResult);
   });
+  test('p256 encrypt/decrypt ECIES', () async {
+    const mne =
+        'drama catch young miss please high blanket animal armor outdoor capital page';
+
+    final key1 = await getECKeysAsync(mne);
+    final key2 = await getECKeysAsync(mne, index: 1);
+
+    final id1 = await P256Identity.fromSecretKey(key1.ecPrivateKey!);
+    final id2 = await P256Identity.fromSecretKey(key2.ecPrivateKey!);
+
+    print(id1.getPrincipal().toText());
+    print(id2.getPrincipal().toText());
+
+    const message =
+        'usage depart wait tiny depart edit another rebuild ginger panel injury share';
+
+    final encryptedResult = await encryptP256Message(
+      identity: id1,
+      theirPublicKey: id2.getPublicKey(),
+      text: message,
+    );
+
+    final decryptedResult = await decryptP256Message(
+      identity: id2,
+      theirPublicKey: id1.getPublicKey(),
+      cipherText: encryptedResult.content,
+    );
+    expect(message, decryptedResult);
+  });
 }
