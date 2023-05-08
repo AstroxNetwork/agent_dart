AstroxNetwork/agent_dart

commit 075665e60b02d613c32357391b47a9e6a17cbed3

✨ Add `canisterId` and `caller` for `PollingResponseException` (#59)

lib/agent/agent/http/index.dart
@@ -164,6 +164,8 @@ class HttpAgent implements Agent {
   @override
   BinaryBlob? rootKey = blobFromHex(_icRootKey);
 
+  Identity? get identity => _identity;
+
   void setPipeline(List<HttpAgentRequestTransformFn> pl) {
     _pipeline = pl;
   }

lib/agent/polling/polling.dart
@@ -10,6 +10,12 @@ Future<BinaryBlob> pollForResponse(
   RequestId requestId,
   PollStrategy strategy,
 ) async {
+  final Principal? caller;
+  if (agent is HttpAgent) {
+    caller = agent.identity?.getPrincipal();
+  } else {
+    caller = null;
+  }
   final path = [blobFromText('request_status'), requestId];
   final state = await agent.readState(
     canisterId,
@@ -47,56 +53,72 @@ Future<BinaryBlob> pollForResponse(
         [...path, blobFromText('reject_message')],
       )!.u8aToString();
       throw PollingResponseRejectedException(
+        canisterId: canisterId,
         requestId: requestIdToHex(requestId),
         status: status,
         rejectCode: rejectCode,
         rejectMessage: rejectMessage,
+        caller: caller,
       );
     case RequestStatusResponseStatus.done:
       // This is _technically_ not an error, but we still didn't see the
       // `Replied` status so we don't know the result and cannot decode it.
       throw PollingResponseDoneException(
+        canisterId: canisterId,
         requestId: requestIdToHex(requestId),
         status: status,
+        caller: caller,
       );
   }
 }
 
 class PollingResponseException implements Exception {
   const PollingResponseException({
+    required this.canisterId,
     required this.requestId,
     required this.status,
+    this.caller,
   });
 
+  final Principal canisterId;
   final String requestId;
   final RequestStatusResponseStatus status;
+  final Principal? caller;
 
   @override
   String toString() {
-    return 'Call was ${status.name}:\n   '
-        'Request ID: $requestId\n';
+    return 'Call was ${status.name}:\n'
+        '|- Canister ID: $requestId\n'
+        '|-- Request ID: $requestId\n'
+        '|------ Caller: $caller';
   }
 }
 
 class PollingResponseDoneException extends PollingResponseException {
   const PollingResponseDoneException({
+    required super.canisterId,
     required super.requestId,
     required super.status,
+    super.caller,
   });
 
   @override
   String toString() {
     return 'Call was marked as ${status.name} but we never saw the reply:\n'
-        '  Request ID: $requestId\n';
+        '|- Canister ID: $requestId\n'
+        '|-- Request ID: $requestId\n'
+        '|------ Caller: $caller';
   }
 }
 
 class PollingResponseRejectedException extends PollingResponseException {
   const PollingResponseRejectedException({
+    required super.canisterId,
     required super.requestId,
     required super.status,
     required this.rejectCode,
     required this.rejectMessage,
+    super.caller,
   });
 
   final BigInt rejectCode;
@@ -105,8 +127,10 @@ class PollingResponseRejectedException extends PollingResponseException {
   @override
   String toString() {
     return 'Call was ${status.name}:\n'
-        '   Request ID: $requestId\n'
-        '  Reject code: $rejectCode\n'
-        '   Reject msg: $rejectMessage\n';
+        '|- Canister ID: $requestId\n'
+        '|-- Request ID: $requestId\n'
+        '|------ Caller: $caller\n'
+        '|-------- Code: $rejectCode\n'
+        '|----- Message: $rejectMessage';
   }
 }

commit 9902973fde77be63223b65b748a2477e8a4b4d65

🚀 Expand `pollForResponse` and agent fetching methods (#62)
Also fixes previous reference issues introduced by #59.

---------

Co-authored-by: iota9star <iota9star@foxmail.com>

lib/agent/actor.dart
@@ -432,6 +432,7 @@ ActorMethod _createActorMethod(Actor actor, String methodName, Func func) {
         ecid,
         requestId,
         pollStrategy,
+        methodName,
       );
 
       if (responseBytes.isNotEmpty) {

lib/agent/agent/http/index.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:agent_dart/agent/agent/http/fetch.dart';
@@ -7,6 +8,27 @@ import 'package:meta/meta.dart';
 import 'package:typed_data/typed_data.dart';
 
 const btoa = base64Encode;
+
+Future<T> withRetry<T>(
+  FutureOr<T> Function() action, {
+  int retryTimes = 3,
+  int retryIntervalMills = 500,
+}) async {
+  assert(retryTimes >= 0);
+  assert(retryIntervalMills >= 0);
+  int times = 0;
+  while (true) {
+    try {
+      return await action();
+    } catch (e) {
+      times++;
+      if (times >= retryTimes) {
+        rethrow;
+      }
+      await Future.delayed(Duration(milliseconds: times * retryIntervalMills));
+    }
+  }
+}
 
 enum FetchMethod {
   get,
@@ -238,18 +260,16 @@ class HttpAgent implements Agent {
 
     final newTransformed = await id!.transformRequest(transformedRequest);
     final body = cbor.cborEncode(newTransformed['body']);
-    final list = await Future.wait([
-      _fetch!(
+    final response = await withRetry(
+      () => _fetch!(
         endpoint: '/api/v2/canister/${ecid.toText()}/call',
         method: FetchMethod.post,
         headers: newTransformed['request']['headers'],
         body: body,
       ),
-      Future.value(requestIdOf(submit.toJson()))
-    ]);
+    );
+    final requestId = requestIdOf(submit.toJson());
 
-    final response = list[0] as Map<String, dynamic>;
-    final requestId = list[1] as Uint8List;
     if (!(response['ok'] as bool)) {
       throw AgentFetchError(
         statusCode: response['statusCode'],
@@ -312,11 +332,13 @@ class HttpAgent implements Agent {
 
     final body = cbor.cborEncode(newTransformed['body']);
 
-    final response = await _fetch!(
-      endpoint: '/api/v2/canister/${canister.toText()}/query',
-      method: FetchMethod.post,
-      headers: newTransformed['request']['headers'],
-      body: body,
+    final response = await withRetry(
+      () => _fetch!(
+        endpoint: '/api/v2/canister/${canister.toText()}/query',
+        method: FetchMethod.post,
+        headers: newTransformed['request']['headers'],
+        body: body,
+      ),
     );
 
     if (!(response['ok'] as bool)) {
@@ -364,11 +386,13 @@ class HttpAgent implements Agent {
     );
 
     final body = cbor.cborEncode(newTransformed['body']);
-    final response = await _fetch!(
-      endpoint: '/api/v2/canister/$canister/read_state',
-      method: FetchMethod.post,
-      headers: newTransformed['request']['headers'],
-      body: body,
+    final response = await withRetry(
+      () => _fetch!(
+        endpoint: '/api/v2/canister/$canister/read_state',
+        method: FetchMethod.post,
+        headers: newTransformed['request']['headers'],
+        body: body,
+      ),
     );
 
     if (!(response['ok'] as bool)) {
@@ -390,10 +414,12 @@ class HttpAgent implements Agent {
 
   @override
   Future<Map> status() async {
-    final response = await _fetch!(
-      endpoint: '/api/v2/status',
-      headers: {},
-      method: FetchMethod.get,
+    final response = await withRetry(
+      () => _fetch!(
+        endpoint: '/api/v2/status',
+        headers: {},
+        method: FetchMethod.get,
+      ),
     );
     if (!(response['ok'] as bool)) {
       throw AgentFetchError(

lib/agent/polling/polling.dart
@@ -9,6 +9,7 @@ Future<BinaryBlob> pollForResponse(
   Principal canisterId,
   RequestId requestId,
   PollStrategy strategy,
+  String method,
 ) async {
   final Principal? caller;
   if (agent is HttpAgent) {
@@ -44,7 +45,7 @@ Future<BinaryBlob> pollForResponse(
     case RequestStatusResponseStatus.processing:
       // Execute the polling strategy, then retry.
       await strategy(canisterId, requestId, status);
-      return pollForResponse(agent, canisterId, requestId, strategy);
+      return pollForResponse(agent, canisterId, requestId, strategy, method);
     case RequestStatusResponseStatus.rejected:
       final rejectCode = cert.lookup(
         [...path, blobFromText('reject_code')],
@@ -56,6 +57,7 @@ Future<BinaryBlob> pollForResponse(
         canisterId: canisterId,
         requestId: requestIdToHex(requestId),
         status: status,
+        method: method,
         rejectCode: rejectCode,
         rejectMessage: rejectMessage,
         caller: caller,
@@ -63,10 +65,11 @@ Future<BinaryBlob> pollForResponse(
     case RequestStatusResponseStatus.done:
       // This is _technically_ not an error, but we still didn't see the
       // `Replied` status so we don't know the result and cannot decode it.
-      throw PollingResponseDoneException(
+      throw PollingResponseNoReplyException(
         canisterId: canisterId,
         requestId: requestIdToHex(requestId),
         status: status,
+        method: method,
         caller: caller,
       );
   }
@@ -77,37 +80,42 @@ class PollingResponseException implements Exception {
     required this.canisterId,
     required this.requestId,
     required this.status,
+    required this.method,
     this.caller,
   });
 
   final Principal canisterId;
   final String requestId;
   final RequestStatusResponseStatus status;
+  final String method;
   final Principal? caller;
 
   @override
   String toString() {
     return 'Call was ${status.name}:\n'
-        '|- Canister ID: $requestId\n'
+        '|- Canister ID: $canisterId\n'
         '|-- Request ID: $requestId\n'
-        '|------ Caller: $caller';
+        '|------ Caller: $caller\n'
+        '|------ Method: $method';
   }
 }
 
-class PollingResponseDoneException extends PollingResponseException {
-  const PollingResponseDoneException({
+class PollingResponseNoReplyException extends PollingResponseException {
+  const PollingResponseNoReplyException({
     required super.canisterId,
     required super.requestId,
     required super.status,
+    required super.method,
     super.caller,
   });
 
   @override
   String toString() {
-    return 'Call was marked as ${status.name} but we never saw the reply:\n'
-        '|- Canister ID: $requestId\n'
+    return 'Call was marked as ${status.name} but never replied:\n'
+        '|- Canister ID: $canisterId\n'
         '|-- Request ID: $requestId\n'
-        '|------ Caller: $caller';
+        '|------ Caller: $caller\n'
+        '|------ Method: $method';
   }
 }
 
@@ -116,6 +124,7 @@ class PollingResponseRejectedException extends PollingResponseException {
     required super.canisterId,
     required super.requestId,
     required super.status,
+    required super.method,
     required this.rejectCode,
     required this.rejectMessage,
     super.caller,
@@ -126,10 +135,7 @@ class PollingResponseRejectedException extends PollingResponseException {
 
   @override
   String toString() {
-    return 'Call was ${status.name}:\n'
-        '|- Canister ID: $requestId\n'
-        '|-- Request ID: $requestId\n'
-        '|------ Caller: $caller\n'
+    return '${super.toString()}\n'
         '|-------- Code: $rejectCode\n'
         '|----- Message: $rejectMessage';
   }

lib/agent/polling/strategy.dart
@@ -9,7 +9,7 @@ typedef PollStrategy = Future<void> Function(
   RequestId requestId,
   RequestStatusResponseStatus status,
 );
-typedef Predicate<T> = Future<T> Function(
+typedef PollPredicate<T> = Future<T> Function(
   Principal canisterId,
   RequestId requestId,
   RequestStatusResponseStatus status,
@@ -23,7 +23,7 @@ PollStrategy defaultStrategy() {
   ]);
 }
 
-Predicate<bool> once() {
+PollPredicate<bool> once() {
   bool first = true;
   return (
     Principal canisterId,
@@ -38,7 +38,7 @@ Predicate<bool> once() {
   };
 }
 
-PollStrategy conditionalDelay(Predicate<bool> condition, int timeInMsec) {
+PollStrategy conditionalDelay(PollPredicate<bool> condition, int timeInMsec) {
   return (
     Principal canisterId,
     RequestId requestId,

pubspec.yaml
@@ -4,7 +4,7 @@ description: |
   a plugin package for dart and flutter apps.
   Developers can build ones to interact with Dfinity's blockchain directly.
 repository: https://github.com/AstroxNetwork/agent_dart
-version: 1.0.0-dev.13
+version: 1.0.0-dev.14
 
 environment:
   sdk: '>=2.17.0 <3.0.0'

test/agent/polling.dart
@@ -1,0 +1,71 @@
+import 'package:agent_dart/agent/agent/http/index.dart';
+import 'package:agent_dart/agent/polling/polling.dart';
+import 'package:agent_dart/principal/principal.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(PollingResponseException, () {
+    test('toString() equalization', () {
+      final canisterId = Principal.fromText('vmyqj-fzaaa-aaaaa-qaaga-cai');
+      const requestId = 'test_id';
+      const status = RequestStatusResponseStatus.rejected;
+      const method = 'test_method';
+      final caller = Principal.anonymous();
+
+      final e1 = PollingResponseException(
+        canisterId: canisterId,
+        requestId: requestId,
+        status: status,
+        method: method,
+        caller: caller,
+      );
+      expect(
+        e1.toString(),
+        equals('''Call was rejected:
+|- Canister ID: vmyqj-fzaaa-aaaaa-qaaga-cai
+|-- Request ID: test_id
+|------ Caller: 2vxsx-fae
+|------ Method: test_method'''),
+      );
+
+      final e2 = PollingResponseNoReplyException(
+        canisterId: canisterId,
+        requestId: requestId,
+        status: status,
+        method: method,
+        caller: caller,
+      );
+      expect(
+        e2.toString(),
+        equals('''Call was marked as rejected but never replied:
+|- Canister ID: vmyqj-fzaaa-aaaaa-qaaga-cai
+|-- Request ID: test_id
+|------ Caller: 2vxsx-fae
+|------ Method: test_method'''),
+      );
+
+      final rejectCode = BigInt.from(5);
+      const rejectMessage =
+          'Canister vmyqj-fzaaa-aaaaa-qaaga-cai trapped explicitly';
+      final e3 = PollingResponseRejectedException(
+        canisterId: canisterId,
+        requestId: requestId,
+        status: status,
+        method: method,
+        caller: caller,
+        rejectCode: rejectCode,
+        rejectMessage: rejectMessage,
+      );
+      expect(
+        e3.toString(),
+        equals('''Call was rejected:
+|- Canister ID: vmyqj-fzaaa-aaaaa-qaaga-cai
+|-- Request ID: test_id
+|------ Caller: 2vxsx-fae
+|------ Method: test_method
+|-------- Code: 5
+|----- Message: Canister vmyqj-fzaaa-aaaaa-qaaga-cai trapped explicitly'''),
+      );
+    });
+  });
+}