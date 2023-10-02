f520bb07ac981cc0f8b500c371719578c6738cf6
Alex Li <github@alexv525.com>
2023-10-03 03:34:40 +0800

    ⬆️ Upgrade and regenerate protobuf

--- scripts/build_protobuf.sh
+++ scripts/build_protobuf.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 rm -rf lib/protobuf &&
 mkdir lib/protobuf &&
-protoc --proto_path=protobuf --dart_out=lib/protobuf protobuf/ic_base_types/pb/v1/types.proto protobuf/ic_ledger/pb/v1/types.proto
+protoc \
+  --proto_path=protobuf \
+  --dart_out=lib/protobuf \
+  protobuf/ic_base_types/pb/v1/types.proto \
+  protobuf/ic_ledger/pb/v1/types.proto
+dart format lib/protobuf
