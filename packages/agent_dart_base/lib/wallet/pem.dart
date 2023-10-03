9412015dc6c116cceb227475dadb612a69dbb33e
Alex Li <github@alexv525.com>
2023-10-03 12:37:24 +0800

    ⬆️ Roll deps (#68)
    (cherry picked from commit cb8b0b4623469ca405b93179948c938b00ee9076)

--- packages/agent_dart_base/lib/wallet/pem.dart
+++ packages/agent_dart_base/lib/wallet/pem.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:agent_dart_base/agent_dart_base.dart';
-import 'package:basic_utils/basic_utils.dart';
-import 'package:pointycastle/asn1.dart';
+import 'package:pointycastle/asn1/object_identifiers.dart';
+import 'package:pointycastle/pointycastle.dart';
+
+const String _beginECPrivateKey = '-----BEGIN EC PRIVATE KEY-----';
+const String _beginPrivateKey = '-----BEGIN PRIVATE KEY-----';
 
 enum KeyType { ed25519, secp265k1 }
 
@@ -15,14 +20,14 @@ class PemFile {
 
 Future<PemFile> getPemFile(String path) async {
   final pem = await File(path).readAsString();
-  if (pem.split(CryptoUtils.BEGIN_PRIVATE_KEY).length > 1) {
+  if (pem.split(_beginPrivateKey).length > 1) {
     return PemFile(
-      pem.split(CryptoUtils.BEGIN_PRIVATE_KEY)[1],
+      pem.split(_beginPrivateKey)[1],
       KeyType.ed25519,
     );
-  } else if (pem.split(CryptoUtils.BEGIN_EC_PRIVATE_KEY).length > 1) {
+  } else if (pem.split(_beginECPrivateKey).length > 1) {
     return PemFile(
-      pem.split(CryptoUtils.BEGIN_EC_PRIVATE_KEY)[1],
+      pem.split(_beginECPrivateKey)[1],
       KeyType.secp265k1,
     );
   }
@@ -46,8 +51,93 @@ Future<Ed25519KeyIdentity> ed25519KeyIdentityFromPem(String pem) async {
 }
 
 Future<Secp256k1KeyIdentity> secp256k1KeyIdentityFromPem(String pem) async {
-  final pem2 = CryptoUtils.BEGIN_EC_PRIVATE_KEY + pem;
-  final key = CryptoUtils.ecPrivateKeyFromPem(pem2).d;
+  final pem2 = _beginECPrivateKey + pem;
+  final key = _ecPrivateKeyFromPem(pem2).d;
   final prvU8a = key!.toU8a();
   return Secp256k1KeyIdentity.fromSecretKey(prvU8a);
+}
+
+ECPrivateKey _ecPrivateKeyFromPem(String pem) {
+  if (pem.isEmpty) {
+    throw ArgumentError('Argument must not be null.');
+  }
+  final bytes = _getBytesFromPEMString(pem);
+  return _ecPrivateKeyFromDerBytes(
+    bytes,
+    pkcs8: pem.startsWith(_beginPrivateKey),
+  );
+}
+
+ECPrivateKey _ecPrivateKeyFromDerBytes(Uint8List bytes, {bool pkcs8 = false}) {
+  var asn1Parser = ASN1Parser(bytes);
+  final topLevelSeq = asn1Parser.nextObject() as ASN1Sequence;
+  String curveName = '';
+  Uint8List x;
+  if (pkcs8) {
+    // Parse the PKCS8 format
+    final innerSeq = topLevelSeq.elements!.elementAt(1) as ASN1Sequence;
+    final b2 = innerSeq.elements!.elementAt(1) as ASN1ObjectIdentifier;
+    final b2Data = b2.objectIdentifierAsString;
+    final b2Curvedata = ObjectIdentifiers.getIdentifierByIdentifier(b2Data);
+    if (b2Curvedata != null) {
+      curveName = b2Curvedata['readableName'];
+    }
+
+    final octetString = topLevelSeq.elements!.elementAt(2) as ASN1OctetString;
+    asn1Parser = ASN1Parser(octetString.valueBytes);
+    final octetStringSeq = asn1Parser.nextObject() as ASN1Sequence;
+    final octetStringKeyData =
+        octetStringSeq.elements!.elementAt(1) as ASN1OctetString;
+
+    x = octetStringKeyData.valueBytes!;
+  } else {
+    // Parse the SEC1 format
+    final privateKeyAsOctetString =
+        topLevelSeq.elements!.elementAt(1) as ASN1OctetString;
+    final choice = topLevelSeq.elements!.elementAt(2);
+    final s = ASN1Sequence();
+    final parser = ASN1Parser(choice.valueBytes);
+    while (parser.hasNext()) {
+      s.add(parser.nextObject());
+    }
+    final curveNameOi = s.elements!.elementAt(0) as ASN1ObjectIdentifier;
+    final data = ObjectIdentifiers.getIdentifierByIdentifier(
+      curveNameOi.objectIdentifierAsString,
+    );
+    if (data != null) {
+      curveName = data['readableName'];
+    }
+
+    x = privateKeyAsOctetString.valueBytes!;
+  }
+
+  BigInt osp2i = BigInt.from(0);
+  for (final byte in x) {
+    osp2i = osp2i << 8;
+    osp2i |= BigInt.from(byte);
+  }
+
+  return ECPrivateKey(osp2i, ECDomainParameters(curveName));
+}
+
+Uint8List _getBytesFromPEMString(String pem, {bool checkHeader = true}) {
+  final lines = LineSplitter.split(pem)
+      .map((line) => line.trim())
+      .where((line) => line.isNotEmpty)
+      .toList();
+  final String base64;
+  if (checkHeader) {
+    if (lines.length < 2 ||
+        !lines.first.startsWith('-----BEGIN') ||
+        !lines.last.startsWith('-----END')) {
+      throw ArgumentError(
+        'The given string does not have the correct '
+        'begin/end markers expected in a PEM file.',
+      );
+    }
+    base64 = lines.sublist(1, lines.length - 1).join();
+  } else {
+    base64 = lines.join();
+  }
+  return Uint8List.fromList(base64Decode(base64));
 }
