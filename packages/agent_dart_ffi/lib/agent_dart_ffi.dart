e66f7bbf1ba8430804d97f56e251c3d4f8b1a88c
Alex Li <github@alexv525.com>
2024-01-19 20:19:42 +0800

    🚸 Get network name from `Network`

--- packages/agent_dart_ffi/lib/agent_dart_ffi.dart
+++ packages/agent_dart_ffi/lib/agent_dart_ffi.dart
@@ -1,2 +1,15 @@
+import 'src/bridge_generated.dart' show Network;
+
 export 'src/bridge_generated.dart';
 export 'src/ffi.dart';
+
+const _networkNameMap = {
+  Network.Bitcoin: 'mainnet',
+  Network.Testnet: 'testnet',
+  Network.Regtest: 'regtest',
+  Network.Signet: 'signet',
+};
+
+extension AgentDartFFIBitcoinNetworkExtension on Network {
+  String get networkName => _networkNameMap[this]!;
+}
