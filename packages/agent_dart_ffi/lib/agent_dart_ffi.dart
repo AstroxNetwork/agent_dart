4006cbb05103e9c7f46147af10f7a4ce0383e793
Alex Li <github@alexv525.com>
2024-02-05 23:35:07 +0800

    🚀 More values for Network

--- packages/agent_dart_ffi/lib/agent_dart_ffi.dart
+++ packages/agent_dart_ffi/lib/agent_dart_ffi.dart
@@ -3,13 +3,15 @@ import 'src/bridge_generated.dart' show Network;
 export 'src/bridge_generated.dart';
 export 'src/ffi.dart';
 
-const _networkNameMap = {
-  Network.Bitcoin: 'mainnet',
-  Network.Testnet: 'testnet',
-  Network.Regtest: 'regtest',
-  Network.Signet: 'signet',
+const _networkNameMap = <Network, Set<String>>{
+  Network.Bitcoin: {'livenet', 'mainnet'},
+  Network.Testnet: {'testnet'},
+  Network.Regtest: {'regtest'},
+  Network.Signet: {'signet'},
 };
 
 extension AgentDartFFIBitcoinNetworkExtension on Network {
-  String get networkName => _networkNameMap[this]!;
+  String get networkName => _networkNameMap[this]!.first;
+
+  bool matches(String? name) => _networkNameMap[this]!.contains(name);
 }
