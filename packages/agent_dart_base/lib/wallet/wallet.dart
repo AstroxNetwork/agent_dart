0fd2ddcaae8ed4ee421242f5c0ee0da26c4f3475
Alex Li <github@alexv525.com>
2024-07-19 20:03:13 +0800

    🔥 Remove Bitcoin Development Kit

--- packages/agent_dart_base/lib/wallet/wallet.dart
+++ packages/agent_dart_base/lib/wallet/wallet.dart
@@ -1,5 +1,3 @@
-export 'btc/bdk/bdk.dart' hide Script;
-export 'btc_wallet.dart';
 export 'hashing.dart';
 export 'keysmith.dart';
 export 'pem.dart';
--- packages/agent_dart_ffi/native/agent_dart/src/bdk/mod.rs
+++
@@ -1,8 +0,0 @@
-#![allow(dead_code)]
-pub mod bip322;
-pub mod blockchain;
-pub mod descriptor;
-pub mod key;
-pub mod psbt;
-pub mod types;
-pub mod wallet;
