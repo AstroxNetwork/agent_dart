9f24519610f7e93cbd658638a6c100d8caad3059
Alex Li <github@alexv525.com>
2024-10-11 14:34:57 +0800

    🧪 Adds actor tests

--- packages/agent_dart_base/test/agent/actor.dart
+++ packages/agent_dart_base/test/agent/actor.dart
@@ -1,7 +1,38 @@
+import 'package:agent_dart_base/agent_dart_base.dart';
+import 'package:test/test.dart';
+
 void main() {
   actorTest();
 }
 
 void actorTest() {
-  /// skip, see https://github.com/dfinity/agent-js/blob/main/packages/agent/src/actor.test.ts
+  test('actor', () async {
+    final agent = HttpAgent(
+      defaultHost: 'icp-api.io',
+      defaultPort: 443,
+      options: const HttpAgentOptions(identity: AnonymousIdentity()),
+    );
+    final idl = IDL.Service({
+      'create_challenge': IDL.Func(
+        [],
+        [
+          IDL.Record({
+            'png_base64': IDL.Text,
+            'challenge_key': IDL.Text,
+          }),
+        ],
+        [],
+      ),
+    });
+    final actor = CanisterActor(
+      ActorConfig(
+        canisterId: Principal.fromText('rdmx6-jaaaa-aaaaa-aaadq-cai'),
+        agent: agent,
+      ),
+      idl,
+    );
+    final result = await actor.getFunc('create_challenge')!.call([]);
+    expect(result, isA<Map>());
+    expect(result['challenge_key'], isA<String>());
+  });
 }
